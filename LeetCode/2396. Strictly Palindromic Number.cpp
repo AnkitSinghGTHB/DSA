b51e2573e7367f12d1d4582579cbfafba06dad46
AnkitSinghGTHB <83086747+AnkitSinghGTHB@users.noreply.github.com>
2025-05-07 17:00:26 +0530

    Create 2396. Strictly Palindromic Number.cpp

---
+++ LeetCode/2396. Strictly Palindromic Number.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    bool isStrictlyPalindromic(int n) {
+        //so do we check for every base?
+        //wait a minute, do i have to convert bases everytime?
+        //wait a minute, wait a minute
+        /*  4 100 11 false
+            5 101 12 11 false
+            6 110 20 12 11 false
+            7 111 21 13 12 11 false
+            8 1000 22 20 13 12 11 false
+            and so on
+        */
+        return false;
+    }
+};
