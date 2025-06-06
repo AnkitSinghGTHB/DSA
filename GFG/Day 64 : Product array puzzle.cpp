7ae641390e0f021216eac3148f63613fc46b4a3b
Ankit Singh <83086747+AnkitSinghGTHB@users.noreply.github.com>
2025-06-06 12:22:30 +0530

    Create Day 64 : Product array puzzle.cpp

---
+++ GFG/Day 64 : Product array puzzle.cpp
@@ -0,0 +1,22 @@
+// User function template for C++
+
+class Solution {
+  public:
+    vector<int> productExceptSelf(vector<int>& arr) {
+        //to do this in O(n) i might uh
+        //get prefix and suffix products
+        //and finally multiply prefix and suffix products
+        int n = arr.size();
+        vector<int> left(n, 1), right(n, 1), res(n);
+        for (int i=1; i<n; ++i){
+            left[i]=arr[i-1]*left[i-1];
+        }
+        for (int i=n-2; i>=0; --i){
+            right[i]=arr[i+1]*right[i+1];
+        }
+        for (int i=0; i<n;++i){
+            res[i]=left[i]*right[i];
+        }
+        return res;
+    }
+};
