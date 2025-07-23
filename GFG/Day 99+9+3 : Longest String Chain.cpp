aeb7f213edf5b1f81220aefc451f7f7b6704707c
Ankit Singh <83086747+AnkitSinghGTHB@users.noreply.github.com>
2025-07-23 19:42:09 +0530

    Create Day 99+9+3 : Longest String Chain.cpp

---
+++ GFG/Day 99+9+3 : Longest String Chain.cpp
@@ -0,0 +1,22 @@
+class Solution {
+  public:
+    int longestStringChain(vector<string>& words) {
+        sort(words.begin(), words.end(), [](const string& a, const string& b) {
+            return a.size() < b.size();
+        });
+        unordered_map<string, int> dp;
+        int max_chain = 1;
+        for (string word : words) {
+            dp[word] = 1;
+            for (int i = 0; i < word.size(); ++i) {
+                string predecessor = word.substr(0, i) + word.substr(i + 1);
+                if (dp.find(predecessor) != dp.end()) {
+                    dp[word] = max(dp[word], dp[predecessor] + 1);
+                    max_chain = max(max_chain, dp[word]);
+                }
+            }
+        }
+        
+        return max_chain;
+    }
+};
