a4d73dce47434292168fbd781884cd5faf8bc7e9
Ankit Singh <83086747+AnkitSinghGTHB@users.noreply.github.com>
2025-09-05 15:28:15 +0530

    Create  Code Testcase Testcase Test Result 2749. Minimum Operations to Make the Integer Zero.cpp

---
+++ LeetCode/Code Testcase Testcase Test Result 2749. Minimum Operations to Make the Integer Zero.cpp
@@ -0,0 +1,11 @@
+class Solution {
+public:
+    int makeTheIntegerZero(int num1, int num2) {
+        for (int k = 1; k <= 60; k++) {
+            long long S = (long long)num1 - (long long)k * num2;
+            if (S < k) continue;
+            if (__builtin_popcountll(S) <= k) return k;
+        }
+        return -1;
+    }
+};
