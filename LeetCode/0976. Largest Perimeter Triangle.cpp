f816a76d78495f5b56cf17c3b415463720f5e2f1
Ankit Singh <83086747+AnkitSinghGTHB@users.noreply.github.com>
2025-09-28 15:50:25 +0530

    Create 0976. Largest Perimeter Triangle.cpp

---
+++ LeetCode/0976. Largest Perimeter Triangle.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    int largestPerimeter(vector<int>& nums) {
+        //this is suspiciously not easy at all
+        sort(nums.begin(), nums.end());
+        for (int i = nums.size()-1; i >= 2; i--) {
+            if (nums[i-2] + nums[i-1] > nums[i]) {
+                return nums[i-2] + nums[i-1] + nums[i];
+            }
+        }
+        return 0;
+    }
+};
