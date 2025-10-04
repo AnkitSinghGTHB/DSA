f6d96c5f35194e348ef1b8c6599b563998d0d55d
Ankit Singh <83086747+AnkitSinghGTHB@users.noreply.github.com>
2025-10-04 15:52:14 +0530

    Create 0011. Container With Most Water.cpp

---
+++ LeetCode/0011. Container With Most Water.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    int maxArea(vector<int>& height) {
+        //bruh, nvm
+        //area b/w 2 lines = min(height[i], height[j]) * (j-i)
+        int left = 0;
+        int right = height.size() - 1;
+        int maxArea = 0;
+        while (left < right) {
+            int currentArea = min(height[left], height[right]) * (right - left);
+            maxArea = max(maxArea, currentArea);
+            if (height[left] < height[right]) {
+                left++;
+            } 
+            else {
+                right--;
+            }
+        }
+        return maxArea;
+    }
+};
