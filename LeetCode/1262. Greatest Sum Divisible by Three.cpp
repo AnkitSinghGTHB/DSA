4ae69146a33180eaa68609bdfc1c3194bbe286aa
Ankit Singh <83086747+AnkitSinghGTHB@users.noreply.github.com>
2025-11-23 14:05:04 +0530

    Create 1262. Greatest Sum Divisible by Three.cpp

---
+++ LeetCode/1262. Greatest Sum Divisible by Three.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    int maxSumDivThree(vector<int>& nums) {
+        vector<int> dp = {0, INT_MIN, INT_MIN};
+        for (int num : nums) {
+            vector<int> temp = dp;
+            for (int i = 0; i < 3; i++) {
+                if (dp[i] != INT_MIN) {
+                    int rem = (i + num) % 3;
+                    temp[rem] = max(temp[rem], dp[i] + num);
+                }
+            }
+            dp = temp;
+        }
+        return dp[0];
+    }
+};
