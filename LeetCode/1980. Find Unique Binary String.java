ea7ec4ef5c62024a37e7fc10805d1c23630cdcd7
Ankit Singh <83086747+AnkitSinghGTHB@users.noreply.github.com>
2026-03-08 13:18:25 +0530

    Create 1980. Find Unique Binary String.java

---
+++ LeetCode/1980. Find Unique Binary String.java
@@ -0,0 +1,11 @@
+class Solution {
+    public String findDifferentBinaryString(String[] nums) {
+        int n = nums.length;
+        StringBuilder result = new StringBuilder();
+        for (int i = 0; i < n; i++) {
+            char c = nums[i].charAt(i);
+            result.append(c == '0' ? '1' : '0');
+        }
+        return result.toString();
+    }
+}
