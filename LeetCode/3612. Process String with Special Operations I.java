71bbb5a2812240ab3f46bf80f04063958776be0d
Ankit Singh <ankitbkp2020@gmail.com>
2026-06-16 16:57:12 +0530

    Create 3612. Process String with Special Operations I.java

---
+++ LeetCode/3612. Process String with Special Operations I.java
@@ -0,0 +1,23 @@
+class Solution {
+    public String processStr(String s) {
+        StringBuilder res = new StringBuilder();
+        for (char c : s.toCharArray()) {
+            if (c >= 'a' && c <= 'z') {
+                res.append(c);
+            } 
+            else if (c == '*') {
+                if (res.length() > 0) {
+                    res.deleteCharAt(res.length() - 1);
+                }
+            } 
+            else if (c == '#') {
+                //duplicate current result
+                res.append(res.toString());
+            } 
+            else if (c == '%') {
+                res.reverse();
+            }
+        }
+        return res.toString();
+    }
+}
