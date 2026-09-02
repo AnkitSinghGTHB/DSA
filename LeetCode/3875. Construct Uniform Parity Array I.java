03bf9b4185a9c323600e47673bb62c6fe2eb4f95
Ankit Singh <ankitbkp2020@gmail.com>
2026-09-02 23:31:32 +0530

    Create 3875. Construct Uniform Parity Array I.java

---
+++ LeetCode/3875. Construct Uniform Parity Array I.java
@@ -0,0 +1,9 @@
+class Solution {
+    public boolean uniformArray(int[] nums1) {
+        //even + even -> even
+        //odd + odd -> even
+        //even + odd -> odd
+        //okay got it
+        return true;
+    }
+}
