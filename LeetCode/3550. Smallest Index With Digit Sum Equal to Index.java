0c163febcf9fbb528da38886f1085129fb2f660d
Ankit Singh <ankitbkp2020@gmail.com>
2026-09-25 07:33:27 +0530

    Create 3550. Smallest Index With Digit Sum Equal to Index.java

---
+++ LeetCode/3550. Smallest Index With Digit Sum Equal to Index.java
@@ -0,0 +1,18 @@
+class Solution {
+    public int smallestIndex(int[] nums) {
+        int n = nums.length;
+        for (int i=0;i<n;i++){
+            int t=nums[i];
+            int sum=0;
+            while(t>0){
+                int d = t%10;
+                sum+=d;
+                t/=10;
+            }
+            if(sum==i){
+                return i;
+            }
+        }
+        return -1;
+    }
+}
